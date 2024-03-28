5d73a98892b1f80c74217c9ad04921edcdbca8fb
60letamozgovnet <kiriksemenov057@gmail.com>
2024-03-28 15:12:12 +0400

    test commit 4

--- app/src/main/java/com/example/app2/MainActivity.kt
+++ app/src/main/java/com/example/app2/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.app2.databinding.ActivityMainBinding
 
-//TEST COMMIT  3
+//TEST COMMIT  4
 
 
 class MainActivity : AppCompatActivity() {
